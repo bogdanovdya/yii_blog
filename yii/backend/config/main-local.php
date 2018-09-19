924430c6d73bdf878e93e72a7c325b8fce80ab0a
bogdanovdya <40484332+bogdanovdya@users.noreply.github.com>
2018-09-19 20:55:57 +0500

    update

---
+++ yii/backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'qlPPGpv51JRsT92BzP-24fkdDBZRFLct',
+        ],
+    ],
+];
---
+++ yii/frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'DRLddFPUD0ZCqZcq0GG6hV8bHM9pjnhn',
+        ],
+    ],
+];
